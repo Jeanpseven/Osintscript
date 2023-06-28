223334661f5efd49b46afc60c73ed0d821e1233c
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-06-28 16:53:52 -0300

    Update advanced.py

--- advanced.py
+++ advanced.py
@@ -1,36 +1,32 @@
 import requests
 
-def search_social_media_profiles(name):
-    social_media_sites = [
-        "https://www.facebook.com/{}",
-        "https://www.instagram.com/{}",
-        "https://www.twitter.com/{}",
-        "https://www.linkedin.com/in/{}",
-        "https://www.reddit.com/user/{}",
-        "https://www.pinterest.com/{}",
-        "https://www.telegram.me/{}",
-        "https://www.tiktok.com/@{}",
-        "https://www.youtube.com/{}"
-    ]
+def search_profile_on_site(site, username):
+    url = site.format(username)
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"Profile found on {site}: {url}")
+    else:
+        print(f"Profile not found on {site}")
 
-    found_profiles = []
+# Exemplo de uso
+username = input("Digite o nome de usuário: ")
 
-    for site in social_media_sites:
-        url = site.format(name)
-        response = requests.get(url)
-        
-        if response.status_code == 200:
-            found_profiles.append(url)
+# Lista de sites
+sites = [
+    "https://www.facebook.com/{}",
+    "https://www.instagram.com/{}",
+    "https://www.twitter.com/{}",
+    "https://www.linkedin.com/in/{}",
+    "https://www.reddit.com/user/{}",
+    "https://www.pinterest.com/{}",
+    "https://www.telegram.me/{}",
+    "https://www.tiktok.com/@{}",
+    "https://www.youtube.com/{}",
+    "https://www.wattpad.com/user/{}",
+    "https://www.netflix.com/{}",
+    "https://www.github.com/{}"
+]
 
-    return found_profiles
-
-# Exemplo de uso:
-name = input("Digite o nome para pesquisa: ")
-profiles = search_social_media_profiles(name)
-
-if profiles:
-    print("Perfis encontrados nas redes sociais:")
-    for profile in profiles:
-        print(profile)
-else:
-    print("Nenhum perfil encontrado nas redes sociais.")
+# Busca em sites específicos
+for site in sites:
+    search_profile_on_site(site, username)
